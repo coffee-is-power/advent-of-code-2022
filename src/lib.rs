52831c1f634a796f091ba2b768704aebb7403bd3
Tiago Dinis <tiagodinis33@gmail.com>
2022-12-01 09:57:54 +0000

    fix(lib.rs): use format macro

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,7 @@
-pub fn read_input(day: u8) -> String {
-    std::fs::read_to_string("input/day-{day}.txt").unwrap()
+pub fn read_input(day: u8, real: bool) -> String {
+    std::fs::read_to_string(format!(
+        "input/day-{day}{}.txt",
+        if real { "" } else { "-example" }
+    ))
+    .unwrap()
 }
