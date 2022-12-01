99da012665e8ffaa131d0a6812db641da6da70b5
Tiago Dinis <tiagodinis33@gmail.com>
2022-12-01 10:16:55 +0000

    feat: solve day 1

--- src/bin/level1.rs
+++ src/bin/level1.rs
@@ -1,3 +1,19 @@
+use advent_of_code_2022::read_input;
 fn main() {
-    println!("Hello World")
+    let input = read_input(1, true);
+    let mut elfs_foods = input
+        .split("\n\n")
+        .map(|elf| {
+            elf.split_whitespace()
+                .map(|elf| elf.parse::<u64>().unwrap())
+                .sum::<u64>()
+        })
+        .collect::<Vec<_>>();
+    elfs_foods.sort();
+    println!("{elfs_foods:#?}");
+    println!("Solução da Parte 1: {}", elfs_foods.last().unwrap());
+    println!(
+        "Solução da Parte 2: {}",
+        elfs_foods[elfs_foods.len() - 3..].iter().sum::<u64>()
+    )
 }
